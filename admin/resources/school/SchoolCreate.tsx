5daf102f63beec078d84086b7327d8fbc45c169a
amit-s19 <coutamit.s19@gmail.com>
2023-01-09 12:32:44 +0530

    New fixes

--- admin/resources/school/SchoolCreate.tsx
+++ admin/resources/school/SchoolCreate.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import {
   BooleanInput,
   Create,
@@ -10,10 +10,12 @@ import {
   SelectInput,
   SimpleForm,
   TextInput,
+  useDataProvider,
   useNotify,
   useRedirect,
 } from "react-admin";
-import { getLocationDetails } from "../../utils/LocationDetailsHelper";
+import { useQuery } from "react-query";
+import * as _ from "lodash";
 
 const SchoolCreate = () => {
   // Input Constraints
@@ -36,22 +38,92 @@ const SchoolCreate = () => {
     notify(`School added successfully`);
     redirect(`/school`);
   };
-  const { districts, blocks, clusters } = getLocationDetails();
+
+  const [selectedDistrict, setSelectedDistrict] = useState('');
+  const [selectedBlock, setSelectedBlock] = useState('');
+  const [selectedCluster, setSelectedCluster] = useState('');
+  const dataProvider = useDataProvider();
+  const {
+    data: _districtData,
+    isLoading,
+    error,
+  } = useQuery(["location", "getList", {}], () =>
+    dataProvider.getList("location", {
+      pagination: { perPage: 10000, page: 1 },
+      sort: { field: "id", order: "asc" },
+      filter: {},
+    })
+  );
+
+  const districtData = useMemo(() => {
+    return _districtData?.data;
+  }, [_districtData]);
+
+  const districts = useMemo(() => {
+    if (!districtData) {
+      return [];
+    }
+    return _.uniqBy(districtData, "district").map((a) => {
+      return {
+        id: a.district,
+        name: a.district,
+      };
+    }).sort((a: any, b: any) => { return a.name < b.name ? -1 : 1 });;
+  }, [districtData]);
+
+  const blocks = useMemo(() => {
+    if (!selectedDistrict || !districtData) {
+      return [];
+    }
+
+    return _.uniqBy(
+      districtData.filter((d) => d.district === selectedDistrict),
+      "block"
+    ).map((a) => {
+      return {
+        id: a.block,
+        name: a.block,
+      };
+    }).sort((a: any, b: any) => { return a.name < b.name ? -1 : 1 });;
+  }, [selectedDistrict, districtData]);
+
+  const clusters = useMemo(() => {
+    if (!selectedBlock || !districtData) {
+      return [];
+    }
+    return _.uniqBy(
+      districtData.filter((d) => d.block === selectedBlock),
+      "cluster"
+    ).map((a) => {
+      return {
+        id: a.cluster,
+        name: a.cluster,
+      };
+    }).sort((a: any, b: any) => { return a.name < b.name ? -1 : 1 });;
+  }, [selectedBlock, districtData]);
+
 
   return (
     <Create mutationOptions={{ onSuccess }}>
       <SimpleForm>
         <TextInput source="name" validate={inputConstraints.fullName} />
         <NumberInput source="udise" validate={required("Please enter a valid UDISE")} />
-        <SelectInput label="District" source="location.data.district" choices={districts} validate={inputConstraints.district} />
-        <SelectInput label="Block" source="location.data.block" choices={blocks} validate={inputConstraints.block} />
-        <SelectInput label="Cluster" source="location.data.cluster" choices={clusters} validate={inputConstraints.cluster} />
+        <SelectInput label="District" source="location.data.district" onChange={(e: any) => {
+          setSelectedDistrict(e.target.value);
+          setSelectedBlock('');
+          setSelectedCluster('');
+        }} choices={districts} validate={inputConstraints.district} />
+        <SelectInput label="Block" source="location.data.block" onChange={(e) => {
+          setSelectedBlock(e.target.value);
+          setSelectedCluster('');
+        }} choices={blocks} validate={inputConstraints.block} />
+        <SelectInput label="Cluster" source="location.data.cluster" onChange={(e) => setSelectedCluster(e.target.value)} choices={clusters} validate={inputConstraints.cluster} />
         <SelectInput source="session" label="Session" choices={["S", "W"].map(el => { return { id: el, name: el } })} validate={inputConstraints.session} />
         <SelectInput source="type" label="Type" choices={["GPS", "GMS", "GHS", "GSSS"].map(el => { return { id: el, name: el } })} validate={inputConstraints.type} />
         <BooleanInput source="is_active" />
-        <TextInput source="latitude" validate={inputConstraints.coord} />
-        <TextInput source="longitude" validate={inputConstraints.coord} />
-        <TextInput source="enroll_count" validate={[required("Please enter a valid enroll count"), regex(/^[0-9]\d*$/, "Only Integer Value is allowed."), maxLength(10, "Unable to Enroll this many students.")]} />
+        <TextInput source="latitude" />
+        <TextInput source="longitude" />
+        <TextInput source="enroll_count" />
       </SimpleForm>
     </Create>
   );
